5178125b307cf69bcf54354e5c57e47ef2841147
letife327 <mletife327@gmail.com>
2024-10-07 17:33:15 +0400

    created task 7

---
+++ task_7/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        if (a%2==0){
+            System.out.println(a+=2);
+        }else {
+            System.out.println(a+=1);
+        }
+
+    }
+}
