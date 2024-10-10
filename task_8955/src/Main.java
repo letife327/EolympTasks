1fe792d1455d9ba46ffda04b701385e41535320e
letife327 <mletife327@gmail.com>
2024-10-10 16:24:43 +0400

    was written 8955

--- task_8955/src/Main.java
+++ task_8955/src/Main.java
@@ -3,32 +3,39 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
-        long count = 0;
-        long[] arr= new long[(int) n];
-        long[] newArr = new long[(int) count];
+        int n = sc.nextInt();
+        int count = 0;
+        int[] arr= new int[ n];
+
         for(int i=0;i<n;i++){
-            long random = sc.nextLong();
+            int random = sc.nextInt();
             arr[i]= random;
 
         }
-        for(long i=0 ; i<arr.length ;i++){
-                if(arr[(int) i]>0){
-                newArr[(int) count] = arr[(int) i];
+        for(int i=0 ; i<arr.length ;i++){
+                if(arr[i]>0){
                 count++;
                }
 
         }
-
-
+        int newIntIndex = 0;
+        int[] newArr = new int[count];
+        for(int i=0 ; i<arr.length ;i++){
+            if(arr[i]>0){
+                newArr[newIntIndex]=arr[i];
+                newIntIndex++;
+            }
+        }
         if(count>0){
             System.out.println(count);
-            for(long j=0 ; j<newArr.length ;j++){
-                System.out.print(newArr [(int) j]);
+            for (int i=0;i<newArr.length;i++){
+                System.out.print(newArr[i]+" ");
             }
-
         }else{
             System.out.println("NO");
         }
+
+
+
     }
 }
